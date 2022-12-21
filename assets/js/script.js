2eae0b667599f61906084b676e82e762aa6fa6fe
Bvivg <tatytygulovbatyrbek19@gmail.com>
2022-12-21 15:37:05 +0600

    exam-footer

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.nav__collapse');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  menu.classList.toggle('is-active');
+});
+let burger = document.querySelector('.burger');
+let bannerMenu = document.querySelector('.banner__nav');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-active');
+  bannerMenu.classList.toggle('is-active');
+});
+
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+    el: ".swiper-pagination",
+        },
+      });
